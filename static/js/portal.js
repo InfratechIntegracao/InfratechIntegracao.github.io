27c3519a8f360480197cf1d203b49896764a1f0a
João Victor <victor.oliveira6@ftc.edu.br>
2024-05-14 00:49:44 -0300

    create pages inicio, controle Obras, Controle Materiais, Relatorio

---
+++ static/js/portal.js
@@ -0,0 +1,11 @@
+function controleO() {
+    window.location.href = "/templates/controledeobras.html"
+}
+
+function controleM() {
+    window.location.href = "/templates/controledemateriais.html"
+}
+
+function gerarelat() {
+    window.location.href = "/templates/relatorio.html"
+}
