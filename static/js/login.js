ff7fae0ce7c2b818d7191f201478c4e60c4ed73e
João Victor <victor.oliveira6@ftc.edu.br>
2024-05-20 00:41:17 -0300

    Feat/Site Completo

--- static/js/login.js
+++ static/js/login.js
@@ -1,9 +1,28 @@
 
 function voltarParaIndex() {
   window.location.href = "/inicio.html";
-}
+};
 
-function portal() {
-  window.location.href ="/portal.html"
+document.getElementById('formLogin').addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede a submissão padrão do formulário
+
+  // Obtém os valores dos campos de entrada
+  const username = document.getElementById('login').value;
+  const password = document.getElementById('senha').value;
+
+  // Verifica as credenciais
+  if (username === 'infratech@gmail.com' && password === '123') {
+      // Redireciona para o portal
+      window.location.href = '/portal.html';
+  } else {
+      // Exibe uma mensagem de erro (opcional)
+      alert('Credenciais inválidas. Tente novamente.');
+      window.location.href = '/login.html'
+  }
+});
+
+function removerValores() {
+    document.getElementById('login').value = '';
+    document.getElementById('senha').value = '';
 }
 
